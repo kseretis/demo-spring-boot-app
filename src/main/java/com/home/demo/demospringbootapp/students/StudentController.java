a62ce47c8ae87437461caada0c5821af343f0544
kseretis <serkleanthis@gmail.com>
2023-02-07 15:13:30 +0200

    update method fixed

--- src/main/java/com/home/demo/demospringbootapp/students/StudentController.java
+++ src/main/java/com/home/demo/demospringbootapp/students/StudentController.java
@@ -3,6 +3,7 @@ package com.home.demo.demospringbootapp.students;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,11 @@ public class StudentController {
 	@RequestMapping(value = "/students", method = RequestMethod.POST)
 	public void addStudent(@RequestBody Student student) {
 		studentService.addStudent(student);
+	}
+	
+	@RequestMapping(value = "/students/{id}", method = RequestMethod.PUT)
+	public void updateStudent(@PathVariable Integer id,@RequestBody Student student) {
+		studentService.updateStudent(id, student);
 	}
 
 }
--- src/main/java/com/home/demo/demospringbootapp/students/StudentService.java
+++ src/main/java/com/home/demo/demospringbootapp/students/StudentService.java
@@ -2,6 +2,7 @@ package com.home.demo.demospringbootapp.students;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +20,17 @@ public class StudentService {
 	public List<Student> getAllStudents(){
 		List<Student> students = new ArrayList<>();
 		studentRepository.findAll().forEach(students::add);
-		logger.info("Students: {}", students);
 		return students;
 	}
 	
 	public void addStudent(Student student) {
+		studentRepository.save(student);
+	}
+	
+	public void updateStudent(Integer id, Student student) {
+		Optional<Student> studentById = studentRepository.findById(id);
+		logger.info("find by id student --> {}", studentById.toString());
+		student.setId(studentById.get().getId());
 		studentRepository.save(student);
 	}
 
