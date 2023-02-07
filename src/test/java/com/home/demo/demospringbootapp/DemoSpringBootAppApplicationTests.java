f16b3f71390cd1aa39b83587d5256eab2ea24064
kseretis <serkleanthis@gmail.com>
2023-02-07 10:32:01 +0200

    init commit

---
+++ src/test/java/com/home/demo/demospringbootapp/DemoSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.demo.demospringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
